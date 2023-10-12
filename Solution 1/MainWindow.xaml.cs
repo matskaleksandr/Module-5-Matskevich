dafebc40f9098a6b68301c28b4930de4428ff44f
AleksandrMatskevich <kokoseke06@gmail.com>
2023-10-12 11:38:42 +0300

    интерфейс

--- Solution 1/MainWindow.xaml.cs
+++ Solution 1/MainWindow.xaml.cs
@@ -1,69 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.IO;
 using Microsoft.Win32;
 
 namespace Solution_1
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Главное окно приложения
     /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
+            //инициализация компонентов окна
             InitializeComponent();
         }
-
+        //обработчик нажатия на кнопку сохранения
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            //создание диалогового окна для сохранения файла
             SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.Filter = "Text files (*.txt)|All files (*.*)";
-            if(saveFileDialog.ShowDialog() == true)
+            saveFileDialog.Filter = "Текстовые файлы (*.txt)|Все файлы (*.*)";
+            if (saveFileDialog.ShowDialog() == true)
             {
                 try
                 {
                     string filepath = saveFileDialog.FileName;
                     TextRange textrange = new TextRange(richtext.Document.ContentStart, richtext.Document.ContentEnd);
+                    //сохранение содержимого в файл
                     using (FileStream fs = new FileStream(filepath, FileMode.Create))
                     {
-                        textrange.Save(fs,DataFormats.Text);
+                        textrange.Save(fs, DataFormats.Text);
                     }
                 }
-                catch(Exception ex)
+                catch (Exception ex)
                 {
+                    //вывод сообщения об ошибке в случае исключения
                     MessageBox.Show(ex.Message);
                 }
             }
         }
-
+        //обработчик нажатия на кнопку открытия файла
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
+            //создание диалогового окна для открытия файла
             OpenFileDialog openFileDialog = new OpenFileDialog();
-            if(openFileDialog.ShowDialog() == true)
+            if (openFileDialog.ShowDialog() == true)
             {
                 try
                 {
                     string filepath = openFileDialog.FileName;
                     string content = File.ReadAllText(filepath);
+                    //очистка и заполнение RichTextBox с содержимым файла
                     richtext.Document.Blocks.Clear();
                     richtext.Document.Blocks.Add(new Paragraph(new Run(content)));
                 }
-                catch(Exception ex)
-                { 
-                    MessageBox.Show(ex.Message); 
+                catch (Exception ex)
+                {
+                    //вывод сообщения об ошибке в случае исключения
+                    MessageBox.Show(ex.Message);
                 }
             }
         }
